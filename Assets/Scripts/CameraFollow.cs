c9f671d7084f5a5f3e7068e58ef3a78363cd1fce
Michal Talaga <mikeon@objectware.pl>
2022-04-18 22:34:37 +0100

    camera follows player but is jerky

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
-
+using System.Linq;
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] Transform target;
 
     private void FixedUpdate()
     {
-        if (target == null) return;
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectsWithTag("Player").SingleOrDefault(p => p.GetComponent<PlayerMovement>().IsLocalPlayer)?.transform;
+            if (target == null) return;
+        }
         var transform = this.transform;
         var desiredPosition = target.position;
         desiredPosition.z = transform.position.z;
