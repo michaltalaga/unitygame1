8259d071e7c9a2f48b021f4069a7b164f9297bd2
Michal Talaga <mikeon@objectware.pl>
2022-04-18 23:26:49 +0100

    shooting from bullet spawn point

--- Assets/Scripts/FireWeapon.cs
+++ Assets/Scripts/FireWeapon.cs
@@ -8,6 +8,7 @@ public class FireWeapon : NetworkBehaviour
 {
     new Collider2D collider2D;
     [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletSpawnPoint;
     
     float lastBulletTime = 0;
     void Start()
@@ -22,9 +23,9 @@ public class FireWeapon : NetworkBehaviour
         {
             lastBulletTime = Time.time;
             var newBullet = Instantiate(bullet);
-            newBullet.transform.position = collider2D.bounds.center;
+            newBullet.transform.position = bulletSpawnPoint.position;
             var bulletBody = newBullet.GetComponent<Rigidbody2D>();
-            var direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - collider2D.bounds.center;
+            var direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - bulletSpawnPoint.position;
             direction = direction * 100000;
             direction = Vector2.ClampMagnitude(direction, 10);
             bulletBody.velocity = direction;
